09a975840e7ef242a2f7ec9d92af2656a8fa54a7
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-12-09 19:20:13 -0500

    Se agrego el 1er ejercicio de condicionales simples

---
+++ lote.bat
@@ -0,0 +1,3 @@
+python Rufasto_Torres/condicionales.simples.py "edgar" "calculo" 13
+python Rufasto_Torres/condicionales.simples.py "jimmy" "econometria" 15
+python Rufasto_Torres/condicionales.simples.py "luis" "ingles" 18
