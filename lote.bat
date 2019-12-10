31d35541e8578147f470d1837553b7e30f30f188
Diana-rufasto <57873525+diana-rufasto@users.noreply.github.com>
2019-12-09 19:26:18 -0500

    Se agrego el 2do ejercicio de condicionales simples

--- lote.bat
+++ lote.bat
@@ -1,3 +1,7 @@
 python Rufasto_Torres/condicionales.simples.py "edgar" "calculo" 13
 python Rufasto_Torres/condicionales.simples.py "jimmy" "econometria" 15
 python Rufasto_Torres/condicionales.simples.py "luis" "ingles" 18
+
+python Rufasto_Torres/Condicionales.simples.py "irma" 56.50 1.58 27.54
+python Rufasto_Torres/Condicionales.simples.py "maria" 66.45 1.65 28.44
+python Rufasto_Torres/Condicionales.simples.py "wilmer" 70.34 1.63 30.12
